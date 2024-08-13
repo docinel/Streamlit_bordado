e899251082c2c260fb93b991d4bbd3eea48d6cdd
docinel <docinel@gmail.com>
2024-08-13 15:06:02 -0300

    Alteração ma estrutura para localizar as imagens

--- bordado.py
+++ bordado.py
@@ -6,7 +6,13 @@ st.set_page_config(
     page_icon="👋",
 )
 
-imagens = r'C:\Users\rodrigo.docinel\Documents\Streamlit_bordado\images'
+# Acessar a pasta contendo as imagens
+os.chdir(r'C:\Users\rodrigo.docinel\Documents\Streamlit_bordado')
+local = os.getcwd()
+
+# Caminho para as imagens
+images = os.path.join(local, 'images')
+
 
 st.markdown(
     """
@@ -22,11 +28,11 @@ st.markdown(
 st.markdown("<h1 style='text-align: center; color: white;'>Bordado</h1>", unsafe_allow_html=True)
 
 # Listar os arquivos na pasta
-arquivos = [arquivo for arquivo in os.listdir(imagens) if os.path.isfile(os.path.join(imagens, arquivo))]
+arquivos = os.listdir(images)
 
 # Selecionar o arquivo
 arquivo_selecionado = st.selectbox("Selecione o arquivo:", arquivos)
 
 # Carregar a imagem
-imagem = os.path.join(imagens, arquivo_selecionado)
+imagem = os.path.join(images, arquivo_selecionado)
 imagem = st.image(imagem, use_column_width=True)
